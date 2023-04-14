6f12397b5bf2ecc7de46472aa7a939163001741a
vlad37419 <85108281+vlad37419@users.noreply.github.com>
2023-04-14 17:45:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/normalize.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Реабилитационный центр Морской</title>
+</head>
+<body>
+<div class="contraindications">
+    <div class="contraindications__container container">
+        <p class="contraindications__text">
+            Имеются противопоказания, необходимо проконсультироваться со специалистом&nbsp;18+
+        </p>
+    </div>
+</div>
+<header class="header">
+    <div class="header__container container">
+        <div class="header__top">
+            <a href="/" class="header__logo logo">
+                <picture class="logo__picture">
+                    <img src="/assets/img/logo.svg" alt="Логотип" class="logo__img">
+                </picture>
+            </a>
+            <div class="header__block">
+                <div class="header__info-block">
+                    <div class="header__block-icon header__block-icon_height"></div>
+                    <p class="header__block-descr">
+                        Время работы:
+                    </p>
+                    <p class="header__block-descr header__block-descr_weight">
+                        Ежедневно 24/7
+                    </p>
+                </div>
+                <button class="header__info-block">
+                    <div class="header__block-icon">
+                        <img src="/assets/img/geo.svg" alt="" aria-label="presentation">
+                    </div>
+                    <p class="header__block-descr">
+                        Краснодар, Станица Динская, улица кирпичная, дом 32
+                    </p>
+                </button>
+            </div>
+            <div class="header__block">
+                <form action="/search-1/" method="post" enctype="multipart/form-data" class="header__search search">
+                    <label class="search__label">
+                        <input name="s" placeholder="Поиск по сайту" type="text" class="search__input">
+                    </label>
+                    <button type="submit" class="search__btn">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17 17L13.1333 13.1333M15.2222 8.11111C15.2222 12.0385 12.0385 15.2222 8.11111 15.2222C4.18375 15.2222 1 12.0385 1 8.11111C1 4.18375 4.18375 1 8.11111 1C12.0385 1 15.2222 4.18375 15.2222 8.11111Z" stroke="#63728C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                    </button>
+                </form>
+                <div class="header__btn-group">
+                    <button class="header__btn btn">
+                        Срочный вызов врача
+                    </button>
+                    <button class="header__btn btn btn_bg-transparent">
+                        Проконсультироваться
+                    </button>
+                </div>
+            </div>
+            <div class="header__block">
+                <p class="header__block-descr header__block-descr_right">
+                    Анонимно 100%
+                </p>
+                <div class="header__connection">
+                    <ul class="header__socials socials">
+                        <li class="socials__item social">
+                            <a href="#" target="_blank" class="social__link">
+                                <img src="/assets/img/socials/vk.svg" alt="" class="social__icon" aria-label="presentation">
+                            </a>
+                        </li>
+                    </ul>
+                    <a href="tel:89064262857" class="header__phone">
+                        <div class="header__phone-icon">
+                            <img src="/assets/img/phone.svg" alt="" aria-label="presentation">
+                        </div>
+                        <p class="header__phone-text">+7 (906) 426-28-57</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header__bottom">
+            <nav class="header__menu menu">
+                <ul class="menu__list">
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Наш центр</span>
+                            <span class="menu__item-icon"></span>
+                        </a>
+                        <ul class="menu__sub-list">
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Врачи
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Сертификаты
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Отзывы
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Фотогалерея
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Частозадаваемые вопросы
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Наркологическая помощь</span>
+                            <span class="menu__item-icon"></span>
+                        </a>
+                        <ul class="menu__sub-list">
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Детоксикация от наркотиков
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Капельница от наркотиков
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Консультация психотерапевта
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Лечение алкоголизма</span>
+                            <span class="menu__item-icon"></span>
+                        </a>
+                        <ul class="menu__sub-list">
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Вывод из запоя
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Капельница от запоя
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Лечение хронического алкоголизма
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Лечение наркомании</span>
+                            <span class="menu__item-icon"></span>
+                        </a>
+                        <ul class="menu__sub-list">
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Принудительное лечение наркозависимых
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Нарколог на дом
+                                </a>
+                            </li>
+                            <li class="menu__sub-item">
+                                <a href="#" class="menu__sub-link">
+                                    Снятие ломки
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Цены</span>
+                        </a>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Публикации</span>
+                        </a>
+                    </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">
+                            <span class="menu__link-text">Контакты</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+</body>
+</html>
